b96a72b9c4964273449d075b880d7aaefca616c4
user1 <satymovaanna@gmail.com>
2018-03-31 15:08:08 +0300

    message

--- task3.py
+++ task3.py
@@ -1,1 +1,12 @@
+import scipy.linalg as linalg
+import numpy as np
+m=15
+A=np.zeros((m,m))
+i,j=np.indices(A.shape)
+A[i == j] = 1
+A[i == j+1] = 1
+A[i == j+2] = 1
 
+B = np.arange(m)
+C = np.linalg.solve(A, B)
+print(C);
